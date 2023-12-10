ad683658c6a537d222cbf3b9cf8cc5cf7de39fa1
Kamihara <hello.camiha@gmail.com>
2023-12-10 13:28:23 +0900

    install telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,4 +11,10 @@ end
 vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
-require("lazy").setup({})
+require("lazy").setup({{
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.5',
+  dependencies = {'nvim-lua/plenary.nvim'},
+  keys = {{"<leader>ff", "<cmd>Telescope find_files<cr>"}, {"<leader>fg", "<cmd>Telescope live_grep<cr>"},
+          {"<leader>fb", "<cmd>Telescope buffers<cr>"}, {"<leader>fh", "<cmd>Telescope help_tags<cr>"}}
+}})
