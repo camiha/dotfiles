629db8df62172f9006766ff18097937d34fe3044
Kamihara <hello.camiha@gmail.com>
2023-12-10 14:19:46 +0900

    add telescope

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,7 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = {'nvim-lua/plenary.nvim'},
+    keys = {{"<leader>ff", "<cmd>Telescope find_files<cr>"}, {"<leader>fg", "<cmd>Telescope live_grep<cr>"},
+            {"<leader>fb", "<cmd>Telescope buffers<cr>"}, {"<leader>fh", "<cmd>Telescope help_tags<cr>"}}
+}
