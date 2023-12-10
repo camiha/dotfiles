62ba5ea26316077b3bf33fc2987da538fb50a794
Kamihara <hello.camiha@gmail.com>
2023-12-10 14:40:51 +0900

    setup neotree

---
+++ .config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,16 @@
+local neotree = {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+        vim.keymap.set("n", "<Leader>b", "<cmd>Neotree reveal position=float<CR>")
+    end
+
+}
+
+return { neotree }
