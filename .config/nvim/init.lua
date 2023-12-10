2c018b90896dd347fab5380825342808fa951a17
Kamihara <hello.camiha@gmail.com>
2023-12-10 13:34:47 +0900

    setup colorscheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,9 +12,14 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
 require("lazy").setup({{
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.5',
-  dependencies = {'nvim-lua/plenary.nvim'},
-  keys = {{"<leader>ff", "<cmd>Telescope find_files<cr>"}, {"<leader>fg", "<cmd>Telescope live_grep<cr>"},
-          {"<leader>fb", "<cmd>Telescope buffers<cr>"}, {"<leader>fh", "<cmd>Telescope help_tags<cr>"}}
+    "cyamy/vim-hybrid",
+    config = function()
+        vim.cmd("colorscheme hybrid")
+    end
+}, {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = {'nvim-lua/plenary.nvim'},
+    keys = {{"<leader>ff", "<cmd>Telescope find_files<cr>"}, {"<leader>fg", "<cmd>Telescope live_grep<cr>"},
+            {"<leader>fb", "<cmd>Telescope buffers<cr>"}, {"<leader>fh", "<cmd>Telescope help_tags<cr>"}}
 }})
