9caa37eeb7f57b276b2882491a3a7f52f5d3129d
Kamihara <hello.camiha@gmail.com>
2023-12-10 14:19:07 +0900

    load plugins from plugin dir

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,23 +3,17 @@ require("keymap")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system(
-        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-         lazypath})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.mapleader = " "
 
-require("lazy").setup({{
-    "cyamy/vim-hybrid",
-    config = function()
-        vim.cmd("colorscheme hybrid")
-    end
-}, {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
-    dependencies = {'nvim-lua/plenary.nvim'},
-    keys = {{"<leader>ff", "<cmd>Telescope find_files<cr>"}, {"<leader>fg", "<cmd>Telescope live_grep<cr>"},
-            {"<leader>fb", "<cmd>Telescope buffers<cr>"}, {"<leader>fh", "<cmd>Telescope help_tags<cr>"}}
-}})
+require("lazy").setup("plugins")
