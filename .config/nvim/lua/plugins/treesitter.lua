d572b4a9540310dec3f241d1e33d75313dffa570
Kamihara <hello.camiha@gmail.com>
2023-12-16 16:01:48 +0900

    update treesitter for astro

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,14 @@
 local treesitter = {
     "nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({
-            with_sync = true,
-            ensure_installed = {"c", "rust", "lua", "vim", "astro", "javascript", "typescript", "css", "scss", "json"}
-        })()
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    config = function()
+        require("nvim-treesitter.configs").setup({ 
+            ensure_installed = {"c", "rust", "lua", "vim", "astro", "javascript", "typescript","tsx", "css", "scss", "json", "html"},
+            highlight = {
+                enable = true,
+              },
+            additional_vim_regex_highlighting = false,
+        })
     end
 }
 
