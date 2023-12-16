0d14d189f99ae3dc1c61aa1c0c1a16778e756ca7
Kamihara <hello.camiha@gmail.com>
2023-12-16 13:52:18 +0900

    update link script

--- link.sh
+++ link.sh
@@ -1,16 +1,14 @@
 
 #!/bin/zsh
 
-create_symlinks() {
+create_symlinks_with_dir() {
     local source_dir=$1
     local target_dir=$2
-
     mkdir -p "$target_dir"
-
     for file in "$source_dir"/*; do
         if [ -d "$file" ]; then
             mkdir -p "$target_dir/$(basename "$file")"
-            create_symlinks "$file" "$target_dir/$(basename "$file")"
+            create_symlinks_with_dir "$file" "$target_dir/$(basename "$file")"
         else
             ln -sf "$file" "$target_dir/$(basename "$file")"
         fi
@@ -21,9 +19,8 @@ NVIM_SOURCE_DIR="$(pwd)/nvim"
 NVIM_TARGET_DIR="$HOME/.config/nvim"
 
 rm -rf "$NVIM_TARGET_DIR"
-create_symlinks "$NVIM_SOURCE_DIR" "$NVIM_TARGET_DIR"
+create_symlinks_with_dir "$NVIM_SOURCE_DIR" "$NVIM_TARGET_DIR"
 
-create_symlinks "$(pwd)/zsh" "$HOME/"
-create_symlinks "$(pwd)/iterm2" "$HOME/.iterm2"
+ln -sf "$(pwd)/zsh/.zshrc" "$HOME/.zshrc"
 
 echo "symlinks created successfully."
