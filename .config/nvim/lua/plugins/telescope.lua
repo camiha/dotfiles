fc3c1534bd22f905297f496e79865957f7270b37
Kamihara <hello.camiha@gmail.com>
2023-12-17 00:30:00 +0900

    add devicon for telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 local telescope = {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.5',
-    dependencies = {'nvim-lua/plenary.nvim'},
+    dependencies = {'nvim-lua/plenary.nvim', "nvim-tree/nvim-web-devicons"},
     keys = {{"<leader>ff", "<cmd>Telescope find_files<cr>"}, {"<leader>fg", "<cmd>Telescope live_grep<cr>"},
             {"<leader>fb", "<cmd>Telescope buffers<cr>"}, {"<leader>fh", "<cmd>Telescope help_tags<cr>"}}
 }
