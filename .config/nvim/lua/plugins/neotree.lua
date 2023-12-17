75600598288812553d64be100c8796b4060015a1
Kamihara <hello.camiha@gmail.com>
2023-12-17 14:29:02 +0900

    remove unused mapps

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -9,9 +9,6 @@ local neotree = {
 	},
 	lazy = true,
   keys = {{"<leader>v", "<cmd>Neotree reveal position=float<CR>"}},
-	config = function()
-		vim.keymap.set("n", "<Leader>b", "<cmd>Neotree reveal position=float<CR>")
-	end,
 }
 
 return { neotree }
